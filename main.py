a98bbd960cd46e459647602b3fa325fa7d410e7c
xdd <xdd@example.com>
2024-09-30 13:28:36 -0300

    program structuring

---
+++ main.py
@@ -0,0 +1,21 @@
+import subprocess
+
+class MPVController:
+    """Handles interactions with MPV commands."""
+    pass
+
+class FileLoader:
+    """Loads files from dir/dirs."""
+    pass
+
+class KeyboardController:
+    """Handles keyboard input."""
+    pass
+
+class PlaylistManager:
+    """Manages playlist."""
+    pass
+
+if  __name__ == "__main__":
+    subprocess.run(["mpv", "--list-options"], shell=True)
+   
