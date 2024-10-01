c68ca94ddca6d12efa764dec7893c927b51e0fc4
xdd <xdd@example.com>
2024-09-30 21:27:25 -0300

    fixed no value error bug

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 import random
 import time
+from collections import deque
 from pymediainfo import MediaInfo
 
 FILE_EXTENSIONS=['mkv', 'mp4', 'ts']
@@ -12,7 +13,8 @@ class MPVController:
     def __init__(self, ipc_path=r'\\.\pipe\mpv-pipe'):
         self._ipc_path = ipc_path
         self._playlist = PlaylistManager().playlist
-        self.mpv_process = subprocess.Popen(['mpv', '--idle', f'--input-ipc-server={self._ipc_path}', '--no-terminal'],
+        self.mpv_process = subprocess.Popen(['mpv', '--idle', f'--input-ipc-server={self._ipc_path}', 
+                                             '--script-opts=autoload-disabled=yes'],
                                        shell=True,
                                        stdin=subprocess.PIPE,
                                        stdout=subprocess.PIPE,
@@ -41,8 +43,28 @@ class MPVController:
             raise e
 
     def run(self):
-        if self.wait_for_pipe():
-            self.send_command(f'loadfile "{self._playlist[0][0]}"')
+        self.wait_for_pipe()
+        _list = deque(self._playlist)
+        print('PLAYLIST LEN', len(self._playlist))
+        # print(self._playlist)
+        while len(_list):
+            try:
+                i_file = _list.popleft()
+                filename = i_file[0]
+                start = i_file[1][0]
+                self.send_command(f'loadfile "{filename}" replace start={start}')   #load current file
+                time.sleep(5)
+                self.send_command(f'loadfile "{filename}" replace start={start}')
+                # time.sleep(2)
+                # self.send_command(f'playlist-next')
+            except IndexError:
+                pass
+
+        # self.send_command(f'loadfile "{self._playlist[0][0]}" replace start={self._playlist[0][1][0]}')
+        # time.sleep(2)
+        # print('start 2nd file', self._playlist[1][1][0])
+        # self.send_command(f'loadfile "{self._playlist[1][0]}" append start={self._playlist[1][1][0]}')
+
                 
 
 class FileLoader:
@@ -62,7 +84,7 @@ class KeyboardController:
 class PlaylistManager:
     """Manages playlist."""
     
-    def __init__(self, length=15, total=100):
+    def __init__(self, length=15, total=500):
         self._length = length
         self._total = total
         self.playlist = self.prepare_playlist()
@@ -92,17 +114,21 @@ class PlaylistManager:
         l = self._total
         _pl_dict = pl_dict
         
-        while l > 0:
+        while l > 0 and len(_pl_dict):
             try:
                 rand_key = random.choice(list(_pl_dict.keys()))
-                key_value = _pl_dict[rand_key]
+                key_value = _pl_dict.get(rand_key)
+                if not key_value:
+                    break
                 i = random.choice(range(len(key_value)))
                 rand_value = key_value.pop(i)
                 out.append((rand_key, rand_value))
                 l -= self._length
             except Exception as e:
+                print(e)
                 pass
         return out
+        
 
 
     def prepare_playlist(self):
